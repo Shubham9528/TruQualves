8d63be040583d016ff37f1d0c5ccfd3da2af6d93
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2026-02-04 01:02:07 +0530

    refactor: Implement custom CORS middleware for dynamic origin handling based on environment variables.

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
 import blogRoutes from "./routes/blogRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import connectDB from "./services/mongodbService.js";
+import corsMiddleware from "./middleware/corsMiddleware.js";
 dotenv.config();
 
 const app = express();
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 await connectDB();
 
 // Middleware
-app.use(cors());
+app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
